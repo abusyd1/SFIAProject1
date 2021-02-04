1305fe56a78db2db789aaaeece202270011af1e9
Ubuntu <ubuntu@ip-172-31-38-16.eu-west-1.compute.internal>
2021-02-04 01:16:37 +0000

    create donegit add .

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from application import app, db
-from application.models import Parent, Player
+from application.models import Parent, Player, Add
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -17,3 +17,16 @@ def home():
 def players():
     if request.method == "GET":
        return render_template("players.html", query=Player.query.all())
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form=Add()
+    if request.method == 'POST':
+        new_player = Player(name=form.name.data, age=form.age.data, position=form.position.data)
+        if not form.validate_on_submit():
+            return render_template('adderror.html', form=form, title="New Player")
+        else:
+            db.session.add(new_player)
+            db.session.commit()
+            return redirect(url_for("players"))
+    return render_template('add.html', form=form, title="New Player")
