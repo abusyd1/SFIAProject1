c181f0c1b9db583e4f9c77276ee834ceab1dc58e
Ubuntu <ubuntu@ip-172-31-38-16.eu-west-1.compute.internal>
2021-02-04 19:09:21 +0000

    trying but failing

--- application/models.py
+++ application/models.py
@@ -15,12 +15,24 @@ class Parent(db.Model):
 
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    club = db.Column(db.String(100), db.ForeignKey('parent.name'), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     position = db.Column(db.String(50), nullable=False)
-    parent_id = db.Column(db.Integer, db.ForeignKey('parent.id'))
 
 class Add(FlaskForm):
     name = StringField('Name', [Length(min=5, max=50)])
     league = StringField('League', [Length(min=5, max = 100)])
     submit = SubmitField('Add Team')
+
+class AddPlayer(FlaskForm):
+    player_name = StringField("Player Name", [Length(min=5, max=50)])
+    player_age = IntegerField("Player Age", validators=[DataRequired(), NumberRange(15,50)])
+    player_position = StringField("Player Position", [Length(min=4, max=50)])
+    submit = SubmitField('Add Player')
+
+class Delete(FlaskForm):
+    submit= SubmitField("Delete Player")
+
+class Update(FlaskForm):
+    submit= SubmitField("Update Player")
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from application import app, db
-from application.models import Parent, Player, Add
+from application.models import Parent, Player, Add, AddPlayer, Delete, Update
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -27,6 +27,23 @@ def add():
             return redirect(url_for("home"))
     return render_template('add.html', form=form, title="New Team")
 
-@app.route('/parent/<Name>', methods = ["GET", "POST"])
+@app.route('/parent/<Name>', methods= ["GET", "POST"])
 def parent(Name):
-    return render_template("update.html")
+   if request.method == "GET":
+       return render_template("players.html", query=Player.query.all())
+
+@app.route('/addplayer/<Name>', methods = ["GET", "POST"])
+def addplayer(Name):
+    form=AddPlayer()
+    if request.method == "POST":
+        new_player = AddPlayer(club=Name, name=form.name.data, age=form.age.data, position=form.position.data)
+        if not form.validate_on_submit():
+            return render_template('addplayererror.html', form=form, title="New Player: "+Name)
+        else:
+            db.session.add(new_player)
+            db.session.commit()
+            return redirect(url_for("parent", Name=new_player.club))
+    return render_template('addplayer.html', form=form, title="New Player: "+Name) 
+    
+
+
