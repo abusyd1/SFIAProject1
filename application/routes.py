c6c25c6dcb5eaeaf177afbf7e5305fcfbffaae03
Ubuntu <ubuntu@ip-172-31-38-16.eu-west-1.compute.internal>
2021-02-04 00:13:03 +0000

    tweaks

--- application/routes.py
+++ application/routes.py
@@ -6,8 +6,14 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+
 @app.route('/home', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def home():
+    return render_template("layout.html")
+
+
+@app.route('/players', methods=['GET', 'POST'])
+def players():
     if request.method == "GET":
-       return render_template("index.html", query=Player.query.all())
+       return render_template("players.html", query=Player.query.all())
