a77524eba20c0e7d5746c1796d580b91fffa721b
Ubuntu <ubuntu@ip-172-31-38-16.eu-west-1.compute.internal>
2021-02-06 16:16:14 +0000

    Dunnit

--- application/routes.py
+++ application/routes.py
@@ -45,25 +45,30 @@ def player(id):
             return redirect(url_for("parent", id=new_player.parent_id))
     return render_template('player.html', form=form)
 
-@app.route('/delete/<id>')
-def delete(id):
-    play = Player.query.filter_by(parent_id=id).first()
-    i = play.parent_id
+@app.route('/delete/<parent_id>/<id>')
+def delete(parent_id, id):
+    play = Player.query.filter_by(id=id).first()
+    i = play.id
+    shay = Player.query.filter_by(parent_id=parent_id).first()
+    j = shay.parent_id
     if play is not None:
         db.session.delete(play)
         db.session.commit()
-        return redirect(url_for("parent", id=i))
+        return redirect(url_for("parent", id=j))
 
 
 
-@app.route('/edit/<id>', methods = ["GET", "POST"])
-def edit(id):
-    form1=Delete()
-    play = Player.query.filter_by(id=id).first()
-    n = play.name
-    a = play.age
-    p = play.position
-    i = play.parent_id
-    if request.method == "POST":
-        return redirect(url_for("delete", id=i))
-    return render_template('delete.html', form1=form1, title=n)
+@app.route('/edit/<parent_id>/<id>', methods = ["GET", "POST"])
+def edit(parent_id, id):
+    form=AddPlayer()
+    playeredit = Player.query.filter_by(id=id).first()
+    if request.method =="POST":
+        playeredit.name=form.name.data
+        playeredit.age=form.age.data
+        playeredit.position=form.position.data
+        if not form.validate_on_submit():
+            return render_template('editerror.html', form=form, playeredit=playeredit)
+        else:
+            db.session.commit()
+            return redirect(url_for("parent", id=playeredit.parent_id))
+    return render_template("edit.html", form=form, playeredit=playeredit)
