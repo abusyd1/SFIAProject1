3c26b2c55f8799cb8f354325b29991031eaf6129
Ubuntu <ubuntu@ip-172-31-38-16.eu-west-1.compute.internal>
2021-02-08 03:46:56 +0000

    testing done!

--- tests/test_app.py
+++ tests/test_app.py
@@ -24,11 +24,13 @@ class TestBase(TestCase):
         db.session.add(testteam2)
         db.session.add(testteam3)
 
+        db.session.commit()
+
         db.session.add_all([
             Player(name="testplayer1", age="15", position="Defender", team=testteam1),
             Player(name="testplayer2", age="16", position="Midfielder", team=testteam2),
             Player(name="testplayer3", age="17", position="Forward", team=testteam3)])
-        db.session.commit
+        db.session.commit()
 
     def tearDown(self):
         db.drop_all()
@@ -37,7 +39,42 @@ class TestAccess(TestBase):
     def test_access_home(self):
         response = self.client.get(url_for('home'))
         self.assertEqual(response.status_code, 200)
+    def test_access_parent(self):
+        response=self.client.get(url_for('parent', id=1))
+        self.assertEqual(response.status_code, 200)
+
+    def test_access_player(self):
+        response=self.client.get(url_for('player', id=1))
+        self.assertEqual(response.status_code, 200)
 
     def test_find_parent(self):
         response = self.client.get(url_for('home'))
         self.assertIn(b'testteam1', response.data)
+
+    def test_find_player(self):
+        response=self.client.get(url_for('parent', id=1))
+        self.assertIn(b'testplayer1', response.data)
+
+class TestAdding(TestBase):
+    def test_add_parent(self):
+        response = self.client.post(url_for('add'), data=dict(name="testaddteam"))
+        self.assertIn(b'testaddteam', response.data)
+
+class TestUpdate(TestBase):
+    def test_update_player(self):
+        response = self.client.post(url_for('edit', parent_id=1, id=1), data=dict(name="hello"))
+        self.assertIn(b'hello', response.data)
+
+class TestDelete(TestBase):
+    def test_delete_player(self):
+        response=self.client.get(url_for('delete', parent_id=1, id=1), data=dict(name="testplayer1"), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+class TestModels(TestBase):
+    def test_parent_model(self):
+        self.assertEqual(Parent.query.count(), 3)
+
+    def test_player_model(self):
+        self.assertEqual(Player.query.count(), 3)
+
+
